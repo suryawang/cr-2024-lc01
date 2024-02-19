217c2588bd38a25417773dec646c5d1410ac21f0
Class <Class@CB204-MHF.binus.local>
2024-02-19 18:22:13 +0700

    large class - extract interface

--- src/the_bloaters/large_class/ExtractInterface.java
+++ src/the_bloaters/large_class/ExtractInterface.java
@@ -15,10 +15,22 @@ public class ExtractInterface {
 	class Employee {
 		private int rate;
 		private boolean hasSpecialSkill;
+		private String name;
+		private String department;
 
-		public Employee(int rate, boolean hasSpecialSkill) {
+		public Employee(int rate, boolean hasSpecialSkill, String name, String department) {
 			this.rate = rate;
 			this.hasSpecialSkill = hasSpecialSkill;
+			this.name = name;
+			this.department = department;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getDepartment() {
+			return department;
 		}
 
 		public int getRate() {
@@ -29,13 +41,15 @@ public class ExtractInterface {
 			return hasSpecialSkill;
 		}
 	}
+
 	void test() {
-		var e1 = new Employee(100, false);
-		var e2 = new Employee(90, true);
+		var e1 = new Employee(100, false, "Budi", "KM");
+		var e2 = new Employee(90, true, "Andi", "IT");
 		var ts = new TimeSheet();
 		System.out.println(ts.charge(e1, 10));
 		System.out.println(ts.charge(e2, 10));
 	}
+
 	public static void main(String[] args) {
 		new ExtractInterface().test();
 	}
