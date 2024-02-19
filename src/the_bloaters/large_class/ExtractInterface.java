ca92d00475b2872f96564cb2be55e97ba35eb69e
Class <Class@CB204-MHF.binus.local>
2024-02-19 18:25:18 +0700

    large class - treatment extract interface

--- src/the_bloaters/large_class/ExtractInterface.java
+++ src/the_bloaters/large_class/ExtractInterface.java
@@ -1,18 +1,20 @@
 package the_bloaters.large_class;
 
 public class ExtractInterface {
+	interface Billable{
+		int getRate();
+		boolean hasSpecialSkill();
+	}
 	class TimeSheet {
-		public double charge(Employee employee, int days) {
-			double base = employee.getRate() * days;
-			if (employee.hasSpecialSkill()) {
+		public double charge(Billable billable, int days) {
+			double base = billable.getRate() * days;
+			if (billable.hasSpecialSkill()) 
 				return base * 1.05;
-			} else {
-				return base;
-			}
+			return base;
 		}
 	}
 
-	class Employee {
+	class Employee implements Billable {
 		private int rate;
 		private boolean hasSpecialSkill;
 		private String name;
