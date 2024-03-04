50f61fe61455d9e631c9776ecc6d824b65f3d6fb
Class <Class@CB204-MHF.binus.local>
2024-03-04 16:11:51 +0700

    OO Abuser - treatment Replace Parameter with explicit method

--- src/oo_abuser/switch_statement/ReplaceParameterWithExplicitMethod.java
+++ src/oo_abuser/switch_statement/ReplaceParameterWithExplicitMethod.java
@@ -2,8 +2,6 @@ package oo_abuser.switch_statement;
 
 public class ReplaceParameterWithExplicitMethod {
 	class Order {
-		public static final int FIXED_DISCOUNT = 0;
-		public static final int PERCENT_DISCOUNT = 1;
 		private int price;
 		private int size;
 
@@ -24,17 +22,11 @@ public class ReplaceParameterWithExplicitMethod {
 		// change apply discount in this Fixed discount and Percent discount
 		// with each own method
 
-		public void applyDiscount(int type, double discount) {
-			switch (type) {
-			case FIXED_DISCOUNT:
-				price -= discount;
-				break;
-			case PERCENT_DISCOUNT:
-				price *= discount;
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid discount type");
-			}
+		public void applyFixedDiscount(double discount) {
+			price -= discount;
+		}
+		public void applyPercentDiscount(double discount) {
+			price *= discount;
 		}
 	}
 
@@ -42,30 +34,30 @@ public class ReplaceParameterWithExplicitMethod {
 		Order order = new Order(20, 4);
 		boolean weekend = false;
 		if (weekend) {
-			order.applyDiscount(Order.FIXED_DISCOUNT, 10);
+			order.applyFixedDiscount(10);
 		}
 		if (order.getSize() > 5) {
-			order.applyDiscount(Order.PERCENT_DISCOUNT, 0.2);
+			order.applyPercentDiscount(0.2);
 		}
 		System.out.println("price = " + order.getPrice());
 
 		Order order1 = new Order(20, 7);
 		boolean weekend1 = true;
 		if (weekend1) {
-			order1.applyDiscount(Order.FIXED_DISCOUNT, 10);
+			order1.applyFixedDiscount(10);
 		}
 		if (order1.getSize() > 5) {
-			order1.applyDiscount(Order.PERCENT_DISCOUNT, 0.2);
+			order1.applyPercentDiscount(0.2);
 		}
 		System.out.println("price = " + order1.getPrice());
 
 		Order order2 = new Order(20, 7);
 		boolean weekend2 = false;
 		if (weekend2) {
-			order2.applyDiscount(Order.FIXED_DISCOUNT, 10);
+			order2.applyFixedDiscount(10);
 		}
 		if (order2.getSize() > 5) {
-			order2.applyDiscount(Order.PERCENT_DISCOUNT, 0.2);
+			order2.applyPercentDiscount(0.2);
 		}
 		System.out.println("price = " + order2.getPrice());
 
