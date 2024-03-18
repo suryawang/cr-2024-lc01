14474fe393eb1bb23cda1398d6248fb900673951
Class <Class@CB204-MHF.binus.local>
2024-03-18 15:51:55 +0700

    the dispensable - comments - extract variable

---
+++ src/the_dispensable/ExtractVariable.java
@@ -0,0 +1,25 @@
+package the_dispensable;
+
+public class ExtractVariable {
+	private int quantity;
+	private int itemPrice;
+
+	double price() {
+		// Price consists of: base price - discount + shipping cost
+		return quantity * itemPrice - Math.max(0, quantity - 500) * itemPrice * 0.05
+				+ Math.min(quantity * itemPrice * 0.1, 100.0);
+	}
+
+	void test() {
+		quantity = 10;
+		itemPrice = 200;
+		System.out.println(price());
+		quantity = 610;
+		itemPrice = 200;
+		System.out.println(price());
+	}
+
+	public static void main(String[] a) {
+		new ExtractVariable().test();
+	}
+}
