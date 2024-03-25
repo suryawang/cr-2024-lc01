f722e091acc80cb790246faec4b4f8e63f5362dd
Class <Class@CB204-MHF.binus.local>
2024-03-25 16:06:27 +0700

    Treatment Introduce Foreign Method

--- src/the_couplers/IntroduceForeignMethod.java
+++ src/the_couplers/IntroduceForeignMethod.java
@@ -7,11 +7,15 @@ public class IntroduceForeignMethod {
 		Date previousDate;
 
 		double schedulePayment() {
-			Date paymentDate = new Date(previousDate.getYear(), previousDate.getMonth(), previousDate.getDate() + 7);
+			Date paymentDate = nextWeek(previousDate);
 
 			// Issue a payment using paymentDate.
 			// ...
 			return 1;
+		}
+
+		public static Date nextWeek(Date date) {
+			return new Date(date.getYear(), date.getMonth(), date.getDate() + 7);
 		}
 	}
 }
